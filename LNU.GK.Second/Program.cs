73a5dddd3cdcb843e89876863a7c293ccc4f888d
AndrewThe <fipsher@gmail.com>
2018-05-07 22:54:32 +0300

    Add project files.

---
+++ LNU.GK.Second/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNU.GK.Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> bound = (t) =>
+            {
+                if (t == 1) return 0;
+                else return 1;
+            };
+
+            Func<double, double> start = (x) =>
+            {
+                if (-1 <= x && x <= 0) return 1;
+                else return 0;
+            };
+
+            var tEnd = 100;
+
+            var solver = new Solver(0.6, tEnd, 1);
+            int n = 100;
+            double a = -1;
+            double b = 1;
+            var step = (b - a) / n;
+            var result = solver.Solve(bound, start, a, b, n);
+
+            var left = new double[n+1];
+            var right = new double[n+1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                left[i] = result[i, 0];
+                right[i] = result[i, n];
+            }
+
+
+            StringBuilder script = new StringBuilder();
+            script.AppendLine($"[X, Y] = meshgrid({a}:{step}:{b}, 0:{tEnd});");
+            script.Append("U = [");
+            for (int i = 0; i <= tEnd; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    var item = result[i, j];
+                    script.Append($"{item} ");
+                }
+                if (i != tEnd)
+                {
+                    script.Append("; ");
+                }
+            }
+            script.Append("];");
+
+            script.AppendLine("figure");
+            script.AppendLine("surf(X,Y,U);");
+            var command = script.ToString();
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ LNU.GK.Second/Solver.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNU.GK.Second
+{
+    public class Solver
+    {
+        private readonly double curant;
+        private readonly int tCount;
+        private double[,] T;
+
+        private double c;
+
+        public Solver(double curant, int tCount, int c)
+        {
+            this.curant = curant;
+            this.tCount = tCount;
+            this.c = c;
+        }
+
+        //private double Predict(int n, int j)
+        //{
+        //    n = n - 1;
+        //    var U = T[n, j] - c * curant * (T[n, j] - T[n, j - 1]);
+
+        //    return U;
+        //}
+        //private double Correct(int n, int j)
+        //{
+        //    n = n - 1;
+        //    var correctnp1 = Predict(n + 1, j);
+        //    var correctnp1jm1 = Predict(n + 1, j - 1);
+        //    var U = 0.5 * ((T[n, j] + correctnp1) - c * curant * (correctnp1 - correctnp1jm1) - c * curant * (T[n, j] - 2 * T[n, j - 1] + T[n, j - 2]));
+
+        //    return U;
+        //}
+
+        private double Predict(int n, int j)
+        {
+            n = n - 1;
+            var U = T[n, j] - c * curant * (T[n, j+1] - T[n, j]);
+
+            return U;
+        }
+        private double Correct(int n, int j)
+        {
+            n = n - 1;
+            var correctnp1 = Predict(n + 1, j);
+            var correctnp1jm1 = Predict(n + 1, j - 1);
+            var U = 0.5 * ((T[n, j] + correctnp1) - c * curant * (correctnp1 - correctnp1jm1));
+
+            return U;
+        }
+
+        public double[,] Solve(
+            Func<double, double> boundary,
+            Func<double, double> start,
+            double a,
+            double b,
+            int n)
+        {
+            var h = (b - a) / n;
+            FillStartT(boundary, start, a, b, n);
+
+            for (int i = 1; i <= tCount; i++)
+            {
+                T[i, 0] = boundary(a);
+                T[i, n] = boundary(b);
+
+                for (int j = 1; j < n; j++)
+                {
+                    //T[i, j] = j == 1 
+                    //    ? Predict(i,j) 
+                    //    : Correct(i, j);
+                    T[i, j] = Correct(i, j);
+                }
+            }
+
+            return (double[,])T.Clone();
+        }
+
+        private void FillStartT(Func<double, double> boundary, Func<double, double> start, double a, double b, int n)
+        {
+            T = new double[tCount + 1, n + 1];
+            var h = (b - a) / n;
+            for (int i = 0; i < n + 1; i++)
+            {
+                T[0, i] = start(a + h * i);
+            }
+            T[0, 0] = boundary(a);
+            T[0, n] = boundary(b);
+        }
+    }
+}
